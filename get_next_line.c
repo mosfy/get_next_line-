7348a8d5d064bd3dcedade006209bf1ea859e524
mosfy <theo.franceschetto@gmail.com>
2025-12-02 18:19:58 +0100

    walla ca passe

--- get_next_line.c
+++ get_next_line.c
@@ -3,72 +3,169 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tfrances <tfrances@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mosfy <mosfy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/24 16:23:27 by tfrances          #+#    #+#             */
-/*   Updated: 2025/11/29 16:41:40 by tfrances         ###   ########.fr       */
+/*   Updated: 2025/12/02 18:13:02 by mosfy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-size_t	ft_strlenline(int fd)
+int	checkbackn(char *s)
 {
 	int	i;
-	char *tmp;
 
 	i = 0;
-	while (read (fd,tmp,1))
+	if (!s)
+		return (1);
+	while (s[i])
 	{
+		if (s[i] == '\n')
+			return (0);
 		i++;
-		if (tmp[0] == '\n')
-			return(i);
 	}
-	return (i);
+	return (1);
+}
+
+char	*ft_strjoin_mod(char *s1, char *s2)
+{
+	int		i;
+	int		j;
+	char	*r;
+
+	if (!s1)
+	{
+		s1 = malloc(1);
+		if (!s1)
+			return (NULL);
+		s1[0] = '\0';
+	}
+	r = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!r)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		r[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j])
+		r[i++] = s2[j++];
+	r[i] = '\0';
+	free(s1);
+	return (r);
+}
+
+char	*extractline(char *save)
+{
+	int		i;
+	char	*line;
+
+	if (!save || save[0] == '\0')
+		return (NULL);
+	i = 0;
+	while (save[i] && save[i] != '\n')
+		i++;
+	line = malloc(i + (save[i] == '\n') + 1);
+	if (!line)
+		return (NULL);
+	i = 0;
+	while (save[i] && save[i] != '\n')
+	{
+		line[i] = save[i];
+		i++;
+	}
+	if (save[i] == '\n')
+		line[i++] = '\n';
+	line[i] = '\0';
+	return (line);
+}
+
+char	*cleansave(char *save)
+{
+	int		i;
+	int		j;
+	char	*sret;
+
+	i = 0;
+	while (save[i] && save[i] != '\n')
+		i++;
+	if (!save[i])
+	{
+		free(save);
+		return (NULL);
+	}
+	i++;
+	sret = malloc(ft_strlen(save) - i + 1);
+	if (!sret)
+	{
+		free(save);
+		return (NULL);
+	}
+	j = 0;
+	while (save[i])
+		sret[j++] = save[i++];
+	sret[j] = '\0';
+	free(save);
+	return (sret);
 }
 
 char *get_next_line(int fd)
 {
-	static char*	tmp;
-	char*			sret;
-	int				i;
-	int				j;
+    static char *save;
+    char tmp[BUFFER_SIZE + 1];
+    int nbread;
+    char *line;
 
-	i = 0;
-	j = 0;
-	tmp = malloc(BUFFER_SIZE + 1);
-	sret = malloc(ft_strlenline(fd));
-	while (read (fd,tmp,BUFFER_SIZE))
-	{
-		while (tmp[i] && tmp[i] != '\n')
-		{
-			sret[j] = tmp[i];
-			i++;
-		}
-		if(tmp[i] == '\n')
-		{
-			sret[j] = '\n';
-			return (sret);
-		}
-	}
+    if (fd < 0 || BUFFER_SIZE <= 0)
+        return (NULL);
+
+    nbread = 1;
+    while (checkbackn(save) && nbread > 0)
+    {
+        nbread = read(fd, tmp, BUFFER_SIZE);
+        if (nbread < 0)
+        {
+            free(save);
+            save = NULL;
+            return (NULL);  // Handle read error
+        }
+
+        if (nbread == 0)  // End of file
+            break;
+
+        tmp[nbread] = '\0';
+        save = ft_strjoin_mod(save, tmp);
+        if (!save)
+            return (NULL);  // Return NULL if memory allocation fails
+    }
+
+    line = extractline(save);  // Extract line from save buffer
+    if (!line)
+        return (NULL);  // Return NULL if no line found
+
+    save = cleansave(save);  // Clean up static buffer
+    return (line);
 }
 
-int main() 
-{
-	int fd;
-	char *line;
 
-	fd = open("mon_fichier.txt", O_RDONLY);
-	if (fd == -1) {
-		return 1;
-	}
+// int	main(void)
+// {
+// 	int		fd;
+// 	char	*line;
 
-	while ((line = get_next_line(fd)) != NULL) 
-	{
-		printf("%s", line);
-		free(line);
-	}
-	close(fd);
-
-	return 0;
-}
+// 	fd = open("mon_fichier.txt", O_RDONLY);
+// 	if (fd == -1)
+// 	{
+// 		return (1);
+// 	}
+// 	while ((line = get_next_line(fd)) != NULL)
+// 	{
+// 		printf("%s", line);
+// 		free(line);
+// 	}
+// 	close(fd);
+// 	return (0);
+// }
--- get_next_line.h
+++ get_next_line.h
@@ -3,21 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tfrances <tfrances@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mosfy <mosfy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/24 16:25:51 by tfrances          #+#    #+#             */
-/*   Updated: 2025/11/29 16:39:25 by tfrances         ###   ########.fr       */
+/*   Updated: 2025/12/02 16:56:37 by mosfy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#pragma once
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 4
-
-#include <unistd.h>
-#include <stdlib.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-
 # endif
+
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/stat.h>
+# include <unistd.h>
+
+size_t	ft_strlen(const char *s);
+char	*get_next_line(int fd);
+
+#endif
